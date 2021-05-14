adb9b6c08a8178577983f14137e2a809f985f89b
kkacilowicz <kacperowekar@gmail.com>
2021-05-14 16:09:15 +0200

    Added Helper and Dto to search for free slot

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -5,6 +5,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Dtos;
 using API.Errors;
+using API.Helpers;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
@@ -54,78 +55,15 @@ namespace API.Controllers
             var LatestTimeAvailable = _activitiesService.GetLatestTimeAvailable(
                 DateTime.Parse(EventsOfUserInCalendar.First().DateStart));
 
-            var StartOfFreeSlot = EarliestTimeAvailable;
-            var EndOfFreeSlot = LatestTimeAvailable;
-            var Gap = new TimeSpan();
+            var TimeSlotAvailable = ActivitiesHelper.SearchForFreeSlot(EventsOfUserInCalendar, EarliestTimeAvailable,
+                LatestTimeAvailable);
 
-            //this loop makes a search in Events of user so as to find a free slot to propose activity
-            for (int i = 0; i < EventsOfUserInCalendar.Count() - 1; i++)
-            {
 
-                var Event = EventsOfUserInCalendar[i];
-
-                if (Event.Name.Length >= 13)
-                {
-                    if (Event.Name.Substring(0, 13) == "[LifeChanger]")
-                        return BadRequest(new ErrorDetails(400, "User has already a LifeChanger event on this day"));
-                }
-
-                var Start = DateTime.Parse(Event.DateStart) + TimeSpan.Parse(Event.TimeStart);
-                var End = DateTime.Parse(Event.DateEnd) + TimeSpan.Parse(Event.TimeEnd);
-
-                if (i == 0 && Start > StartOfFreeSlot)
-                    Gap = Start - StartOfFreeSlot;
-
-                if (End >= EarliestTimeAvailable && Start <= LatestTimeAvailable)
-                {
-                    var EventNext = EventsOfUserInCalendar[i + 1];
-
-                    var StartNext = DateTime.Parse(EventNext.DateStart) + TimeSpan.Parse(EventNext.TimeStart);
-                    var EndNext = DateTime.Parse(EventNext.DateEnd) + TimeSpan.Parse(EventNext.TimeEnd);
-
-                    var GapTemporary = StartNext - End;
-
-                    if (GapTemporary >= Gap)
-                    {
-                        Gap = GapTemporary;
-
-                        StartOfFreeSlot = End;
-                        EndOfFreeSlot = StartNext;
-
-                        if (StartNext >= LatestTimeAvailable)
-                        {
-                            Gap = LatestTimeAvailable - End;
-                            StartOfFreeSlot = End;
-                            EndOfFreeSlot = LatestTimeAvailable;
-                            break; //no more time available, don't need to search more
-                        }
- 
-                    }
-
-                    if (EndNext >= LatestTimeAvailable)
-                        break; //no more time available, don't need to search more
-                }
-
-            }
-            //compute also for last event in list, if last event ends before latest time available
-            if(EndOfFreeSlot != LatestTimeAvailable)
-            {
-                var EventLast = EventsOfUserInCalendar.Last();
-                var EndLast = DateTime.Parse(EventLast.DateEnd) + TimeSpan.Parse(EventLast.TimeEnd);
-                var GapTemporary = LatestTimeAvailable - EndLast;
-
-                if (GapTemporary >= Gap)
-                {
-                    Gap = GapTemporary;
-                    StartOfFreeSlot = EndLast;
-                    EndOfFreeSlot = LatestTimeAvailable;
-                }
-            }
-
-            if((int)Gap.TotalMinutes < 100)
+            if ((int)TimeSlotAvailable.Gap.TotalMinutes < 100)
                 return BadRequest(new ErrorDetails(400, "User has no time for any activities this day"));
 
-            var ListOfActivites = await _activitiesService.GetUserAvailableActivities(user, (int)Gap.TotalMinutes);
+            var ListOfActivites = await _activitiesService.GetUserAvailableActivities(user, 
+                (int)TimeSlotAvailable.Gap.TotalMinutes);
 
             if (ListOfActivites.Count() == 0)
                 return BadRequest(new ErrorDetails(400, "User didn't choose any preferences"));
@@ -138,7 +76,7 @@ namespace API.Controllers
             var ActivityDuration = TimeSpan.FromMinutes(ActivityForUser.AverageTimeInMinutes);
             var ActivityTimeToPrep = TimeSpan.FromMinutes(ActivityForUser.OffsetToPrepare);
 
-            var StartOfActivity = StartOfFreeSlot + ActivityTimeToPrep;
+            var StartOfActivity = TimeSlotAvailable.StartOfFreeSlot + ActivityTimeToPrep;
             var EndOfActivity = StartOfActivity + ActivityDuration;
 
             var ActivityProposed = new ActivityDto
